8369cc7c292b44e9fff4d538bbe67f7c16ca555d
Andrew Askins <r.andrew.askins@gmail.com>
2012-08-25 11:08:34 -0400

    Get funky.

---
+++ Two Thumbs Down/ReViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReViewController.h
+//  Two Thumbs Down
+//
+//  Created by Mac Mini on 8/25/12.
+//  Copyright (c) 2012 It's Okay In A Threeway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReViewController : UIViewController
+
+@end
